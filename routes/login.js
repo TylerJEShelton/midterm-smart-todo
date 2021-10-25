ba73567e56ada5789876c9bcae694c5f88841f9c
yuhaoyann <yuhaoyann@gmail.com>
2021-10-25 19:17:09 -0400

    complete login function

--- routes/login.js
+++ routes/login.js
@@ -9,6 +9,9 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
+  const bcrypt = require("bcrypt");
+  const { getUserByEmail } = require("../lib/data_helpers");
+
   router.get("/", (req, res) => {
     let user = null;
     if (req.session.first_name) {
@@ -19,8 +22,19 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
-    req.session.first_name = "haoyan";
-    res.redirect("/items");
+    let email = req.body.email;
+    getUserByEmail(db, email).then((responce) => {
+      let user = responce.rows[0];
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        req.session.first_name = user.first_name;
+        res.redirect("/items");
+        return;
+      }
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        res.status(403).send("incorrect password");
+        return;
+      }
+    });
   });
 
   router.post("/logout", (req, res) => {
