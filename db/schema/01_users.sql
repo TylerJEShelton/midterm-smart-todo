2415b899894aa74a95dc7f7b583af712d62c89ce
yuhaoyann <yuhaoyann@gmail.com>
2021-10-24 22:21:04 -0400

    change username back to first_name

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(150),
+  first_name VARCHAR(150),
   email VARCHAR(150) NOT NULL,
   password VARCHAR(150) NOT NULL
 );
