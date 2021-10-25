40fa96fdd8d750d48e94cab2df94bf45d9c646c2
yuhaoyann <yuhaoyann@gmail.com>
2021-10-24 21:27:03 -0400

    formatting

--- routes/register.js
+++ routes/register.js
@@ -18,19 +18,19 @@ module.exports = (db) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const queryString = `INSERT INTO users (first_name, email, password) VALUES ($1, $2, $3);`
+    const queryString = `INSERT INTO users (first_name, email, password) VALUES ($1, $2, $3);`;
     const queryParams = [first_name, email, password];
 
-    db.query(queryString, queryParams)
-      .then(() => {
-        db.query(`SELECT id FROM users WHERE email = $1`, [email])
-          .then(data => {
-            const userID = data.rows[0].id;
-            req.session.userID = userID;
-            console.log(req.session.userID);
-            res.redirect("/");
-          })
-      })
+    db.query(queryString, queryParams).then(() => {
+      db.query(`SELECT id FROM users WHERE email = $1`, [email]).then(
+        (data) => {
+          const userID = data.rows[0].id;
+          req.session.userID = userID;
+          console.log(req.session.userID);
+          res.redirect("/");
+        }
+      );
+    });
   });
 
   return router;
