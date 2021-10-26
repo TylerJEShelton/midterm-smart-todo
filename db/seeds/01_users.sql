84ef9d4609840a3a3bd0b207eaf5460be327c7db
yuhaoyann <yuhaoyann@gmail.com>
2021-10-25 21:19:12 -0400

    add instruction for pass

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,5 @@
 -- Users table seeds here
-INSERT INTO users (first_name, email, password) VALUES ('Andriy', 'andriy@mail.com', 'password');
-INSERT INTO users (first_name, email, password) VALUES ('Haoyan', 'haoyan@mail.com', 'password');
-INSERT INTO users (first_name, email, password) VALUES ('Tyler', 'tyler@mail.com', 'password');
+-- password is 'password'
+INSERT INTO users (first_name, email, password) VALUES ('Andriy', 'andriy@mail.com', '$2b$10$rYn0lyHtk/cljVNTa1Il9.QGV2QDAudD1ufu8hNtNMjY5J4.9pAT2');
+INSERT INTO users (first_name, email, password) VALUES ('Haoyan', 'haoyan@mail.com', '$2b$10$rYn0lyHtk/cljVNTa1Il9.QGV2QDAudD1ufu8hNtNMjY5J4.9pAT2');
+INSERT INTO users (first_name, email, password) VALUES ('Tyler', 'tyler@mail.com', '$2b$10$rYn0lyHtk/cljVNTa1Il9.QGV2QDAudD1ufu8hNtNMjY5J4.9pAT2');
