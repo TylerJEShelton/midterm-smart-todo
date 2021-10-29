a7a68ca9cb15169e04f7b148fcca1afd59ab9c63
yuhaoyann <yuhaoyann@gmail.com>
2021-10-29 10:32:03 -0400

    install add item button to the page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,27 @@
 // Client facing scripts here
 $(document).ready(function() {
-  $("#testing").hover(function() {
-      $(this).css("color", "white");
-    });
+  $(".add-from-result1").click(function() {
+    let out = {book1: $("#book1").text()}
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object"
+    })
+  })
+  $(".add-from-result2").click(function() {
+    let out = {movie1: $("#movie1").text()}
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object"
+    })
+  })
+  $(".add-from-result3").click(function() {
+    let out = {restaurant1: $("#restaurant1").text()}
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object"
+    })
+  })
 });
