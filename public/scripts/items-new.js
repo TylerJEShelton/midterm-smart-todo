174600ef41a9f97c3d92de6be65367c5a6076de4
yuhaoyann <yuhaoyann@gmail.com>
2021-10-30 08:38:16 -0400

    show 12 results instead of 3

--- public/scripts/items-new.js
+++ public/scripts/items-new.js
@@ -26,4 +26,85 @@ $(document).ready(function () {
     });
     window.location.replace("/");
   });
+  $(".add-from-result4").click(function () {
+    let out = { book: $("#book2").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result5").click(function () {
+    let out = { movie: $("#movie2").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result6").click(function () {
+    let out = { restaurant: $("#restaurant2").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result7").click(function () {
+    let out = { book: $("#book3").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result8").click(function () {
+    let out = { movie: $("#movie3").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result9").click(function () {
+    let out = { restaurant: $("#restaurant3").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result10").click(function () {
+    let out = { book: $("#book4").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result11").click(function () {
+    let out = { movie: $("#movie4").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
+  $(".add-from-result12").click(function () {
+    let out = { restaurant: $("#restaurant4").text() };
+    $.post({
+      url: "/items/add",
+      data: out,
+      dataType: "Object",
+    });
+    window.location.replace("/");
+  });
 });
